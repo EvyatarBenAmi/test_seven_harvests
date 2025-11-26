cc260dca5000673820261114e1bc24208c447f4a
Evyatar_BenAmi <evyatar2091@gmail.com>
2025-11-26 17:53:13 +0200

    finish time

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from soldiers.reader_CSV import reader_csv_file
 from fastapi import FastAPI
 import uvicorn
 from fastapi import FastAPI,File, UploadFile
+import csv
 
 app = FastAPI()
 
 @app.post("/assignWithCsv")
-def get_file(file: UploadFile = File()):
-        return reader_csv_file(file)
-    
+def reader_csv_file(file: UploadFile = File()) -> list[dict]:
+    text = file.read().decode()
+    reader = csv.DictReader(text.splitlines())
+    list_soldier = [row for row in reader]
+    print(list_soldier)
+    return list_soldier
 
 if __name__ == "__main__":
 
--- soldiers/reader_CSV.py
+++ soldiers/reader_CSV.py
@@ -1,7 +1,15 @@
 import csv
+from sqlmodel import SQLModel
 
-def reader_csv_file(file):
-    text = file.file.read().decode()
-    reader = csv.DictReader(text.splitlines())
-    rows = [row for row in reader]
-    return rows
+def sort_by_distance(list_soldier) -> list[dict]:
+    distance_sorted = list_soldier.sort(key = "distance")
+    return distance_sorted
+
+# class Soldier(SQLModel, table = True):
+    persnal_num : int 
+    first_name : str 
+    last_name : str 
+    gender : str 
+    city : str 
+    distance : int 
+    placement_status : str 
